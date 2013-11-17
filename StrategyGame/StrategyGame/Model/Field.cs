0f10818cc033ad1c818b72720c0dc6dcb833ddd1
nagymarci <nagy90marci@gmail.com>
2013-11-17 17:38:13 +0100

    doksi módositás, program váz létrehozás
    Módosítottam a doksi Unit-os részét (még nem a végleges).
    Létrehoztam egy projektet, és a szükséges fájlokat a teljeség igénye
    nélkül, illetve elkezdtem implementálni a Unit osztályt.

---
+++ StrategyGame/StrategyGame/Model/Field.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyGame.Model
+{
+    //TODO: Implement Field Class
+    class Field
+    {
+    }
+}
