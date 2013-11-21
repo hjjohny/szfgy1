b23925098c67c887926944af72f1ce17b4ded8ea
bodorzolt <nobzol@mpp.hu>
2013-11-21 02:07:26 +0200

    Game-be metódusvázak

--- StrategyGame/StrategyGame/Model/GameManager.cs
+++ StrategyGame/StrategyGame/Model/GameManager.cs
@@ -8,5 +8,43 @@ namespace StrategyGame.Model
     //TODO: Implement GameManager Class
     class GameManager
     {
+        public void buyUnit() {
+            //TODO Implement buyUnit() method
+        }
+
+        public void endTurn()
+        {
+            //TODO Implement endTurn() method
+        }
+
+        public void gameOver()
+        {
+            //TODO Implement gameOver() method
+        }
+
+        public void loadGame()
+        {
+            //TODO Implement loadGame() method
+        }
+
+        public void loadLader()
+        {
+            //TODO Implement loadLader() method
+        }
+
+        public void moveUnit()
+        {
+            //TODO Implement moveUnit() method
+        }
+
+        public void newGame()
+        {
+            //TODO Implement newGame() method
+        }
+
+        public void saveGame()
+        {
+            //TODO Implement saveGame() method
+        }
     }
 }
